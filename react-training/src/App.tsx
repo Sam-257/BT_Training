5e74c452d6ed9ced5f3ad0aaafd93b8586c622e5
Sam-257 <sambhavsurana257@gmail.com>
2022-09-28 14:27:00 +0530

    Todo List and Routing

--- react-training/src/App.tsx
+++ react-training/src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import TodoList from './Components/TodoList';
 import Form from './Components/Form';
-// import Login from './Components/Login';
-// import Demo from './Components/Demo';
-// import Movie from './Components/Movie';
-// import {moviesList} from './Data/movies'
+import Login from './Components/Login';
+import Demo from './Components/Demo';
+import Movie from './Components/Movie';
+import {moviesList} from './Data/movies'
 
 function App() {
   return (
     <div className="App">
-      {/* <Movie moviesList={moviesList} /> */}
-      {/* <Login /> */}
-      <Form />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Demo />} />
+        <Route path="/movie" element={<Movie moviesList={moviesList} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/todo" element={<TodoList />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
