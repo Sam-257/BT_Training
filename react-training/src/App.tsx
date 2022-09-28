3ff4ebb9670dfce47a0cb7f222f74a3c9b884ce0
Sam-257 <sambhavsurana257@gmail.com>
2022-09-28 09:00:48 +0530

    React basics and useState hook

---
+++ react-training/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Form from './Components/Form';
+// import Login from './Components/Login';
+// import Demo from './Components/Demo';
+// import Movie from './Components/Movie';
+// import {moviesList} from './Data/movies'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Movie moviesList={moviesList} /> */}
+      {/* <Login /> */}
+      <Form />
+    </div>
+  );
+}
+
+export default App;
